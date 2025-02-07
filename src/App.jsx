87ce54550299be5bcfbfedb7aba2400f43d841cb
“Piero793” <pierolacitignola7@gmail.com>
2025-02-07 12:02:26 +0100

    navbar+tvshows

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "../components/NavBar";
+import TVShows from "../components/TvShows";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <TVShows />
+    </>
+  );
+}
+
+export default App;
