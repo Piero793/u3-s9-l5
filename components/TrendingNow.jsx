9648e7e58618be184ddbfd6c93dfd8d135b75077
“Piero793” <pierolacitignola7@gmail.com>
2025-02-07 15:25:55 +0100

    spinner

--- components/TrendingNow.jsx
+++ components/TrendingNow.jsx
@@ -4,6 +4,8 @@ import { Container } from "react-bootstrap";
 class TrendingNow extends Component {
   state = {
     movies: [],
+    loading: true, // Stato per lo spinner
+    error: null,
   };
 
   componentDidMount() {
@@ -15,28 +17,43 @@ class TrendingNow extends Component {
       })
       .then((data) => {
         console.log("Dati JSON ok", data);
-        this.setState({ movies: data.Search });
+        this.setState({ movies: data.Search, loading: false });
         console.log("Films", this.state.movies);
       })
       .catch((error) => {
         console.log("Errore durante il fetch dei dati", error);
+        this.setState({ error: error.message, loading: false });
       });
   }
 
   render() {
     console.log("Elenco film", this.state.movies);
+
     return (
       <Container>
-        <h4 className=" text-light mb-4">Trending Now</h4>
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4">
-          {this.state.movies
-            .filter((_, index) => index < 6)
-            .map((movie) => (
-              <div key={movie.imdbID} className="col mb-2 text-center px-1">
-                <img className="img-fluid" src={movie.Poster} alt={movie.Title} />
-              </div>
-            ))}
+        <div className="d-flex align-items-center mb-4">
+          <h4 className="text-light me-2">Trending Now</h4>
+          {/* parte lo spinner*/}
+          {this.state.loading && (
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
         </div>
+
+        {this.state.error ? (
+          <div className="text-center text-light">Errore: {this.state.error}</div>
+        ) : (
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4">
+            {this.state.movies
+              .filter((_, index) => index < 6)
+              .map((movie) => (
+                <div key={movie.imdbID} className="col mb-2 text-center px-1">
+                  <img className="img-fluid" src={movie.Poster} alt={movie.Title} />
+                </div>
+              ))}
+          </div>
+        )}
       </Container>
     );
   }
