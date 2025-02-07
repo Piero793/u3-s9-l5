24e1f8c824e6e7dfda79e192251c74354fd77bf6
“Piero793” <pierolacitignola7@gmail.com>
2025-02-07 12:12:16 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/NavBar";
 import TVShows from "../components/TvShows";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <TVShows />
+      <Footer />
     </>
   );
 }
